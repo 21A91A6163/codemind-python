8cf56f04ff8c21e567e4c425464f24e6c1ab75e1
Navy Navya <97424651+21A91A6163@users.noreply.github.com>
2022-12-23 18:23:18 +0530

    t=int(input()) s=0 q=0 for k in range(t):     n=int(input())     for i in range(n+1,n*n):         c=0         for j in range(1,i+1):             if i%j==0:                 c+=1         if c==2:             s=i             break     for i in range(n,0,-1):         c=0         for j in range(1,i+1):             if i%j==0:                 c+=1         if c==2:             q=i             break     if abs(s-n)<abs(q-n):         print(s)     elif abs(q-n)<abs(s-n):         print(q)     elif abs(s-n)==abs(q-n):         print(q)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,23 +1,27 @@
-def prime(i):
-    if i==1:
-        return 0
-    for j in range(2,int(i**0.5)+1):
-        if i%j==0:
-            return 0
-    else:
-        return 1
-x=int(input())
-for j in range(x):
-    k=int(input())
-    for l in range(k-1,2,-1):
-        if prime(l):
-            h=l
+t=int(input())
+s=0
+q=0
+for k in range(t):
+    n=int(input())
+    for i in range(n+1,n*n):
+        c=0
+        for j in range(1,i+1):
+            if i%j==0:
+                c+=1
+        if c==2:
+            s=i
             break
-    for i in range(k,k+100):
-        if prime(i):
-            z=i
+    for i in range(n,0,-1):
+        c=0
+        for j in range(1,i+1):
+            if i%j==0:
+                c+=1
+        if c==2:
+            q=i
             break
-    if(abs(h-k)<=abs(k-z)):
-        print(h)
-    else:
-        print(z)
+    if abs(s-n)<abs(q-n):
+        print(s)
+    elif abs(q-n)<abs(s-n):
+        print(q)
+    elif abs(s-n)==abs(q-n):
+        print(q)
